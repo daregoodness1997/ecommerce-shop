67ee4510e73ca9c90379864cfe2151a1cbd73b91
daregoodness1997 <daregoodness@gmail.com>
2022-07-02 14:26:27 +0100

    loader and blur effect

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -23,7 +23,11 @@ export const Product = ({ product, handleAddToCart }) => {
     <div>
       <div className='group relative'>
         <div className='w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none'>
-          <ProgressiveImage src={product.assets[0].url} alt={product.name} />
+          <ProgressiveImage
+            src={product.assets[0].url}
+            alt={product.name}
+            placeholderSrc={product.name}
+          />
         </div>
         <div className='mt-4 flex justify-between'>
           <div className='flex flex-col'>
