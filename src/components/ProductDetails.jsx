52178d22810aa29efcee4eca90fbcf31fd195dff
daregoodness1997 <daregoodness@gmail.com>
2022-07-02 22:48:13 +0100

    filter done

--- src/components/ProductDetails.jsx
+++ src/components/ProductDetails.jsx
@@ -1,7 +1,10 @@
-import { useState } from 'react';
+import { useState, useEffect, useContext } from 'react';
 import { StarIcon } from '@heroicons/react/solid';
 import { RadioGroup } from '@headlessui/react';
 import ProgressiveImage from './ProgressiveImage';
+import { useParams } from 'react-router-dom';
+import { DataContext } from '../Context';
+import Loader from './Loader';
 
 const product = {
   name: 'Basic Tee 6-Pack',
@@ -65,54 +68,30 @@ export const ProductDetails = () => {
   const [selectedColor, setSelectedColor] = useState(product.colors[0]);
   const [selectedSize, setSelectedSize] = useState(product.sizes[2]);
 
+  const [productDetails, setProductDetails] = useState({});
+  const value = useContext(DataContext);
+  const [products] = value.products;
+
+  let params = useParams();
+
+  const getProduct = productId => {
+    return products.filter(product => product.id === productId);
+  };
+
+  let productD = getProduct(params.id);
+
+  console.log(params.id, products, productD);
+
+  if (!productD.length) return <Loader />;
+
   return (
     <div className='bg-white'>
       <div className='pt-6'>
-        <nav aria-label='Breadcrumb'>
-          <ol
-            role='list'
-            className='max-w-2xl mx-auto px-4 flex items-center space-x-2 sm:px-6 lg:max-w-7xl lg:px-8'
-          >
-            {product.breadcrumbs.map(breadcrumb => (
-              <li key={breadcrumb.id}>
-                <div className='flex items-center'>
-                  <a
-                    href={breadcrumb.href}
-                    className='mr-2 text-sm font-medium text-gray-900'
-                  >
-                    {breadcrumb.name}
-                  </a>
-                  <svg
-                    width={16}
-                    height={20}
-                    viewBox='0 0 16 20'
-                    fill='currentColor'
-                    xmlns='http://www.w3.org/2000/svg'
-                    aria-hidden='true'
-                    className='w-4 h-5 text-gray-300'
-                  >
-                    <path d='M5.697 4.34L8.98 16.532h1.327L7.025 4.341H5.697z' />
-                  </svg>
-                </div>
-              </li>
-            ))}
-            <li className='text-sm'>
-              <a
-                href={product.href}
-                aria-current='page'
-                className='font-medium text-gray-500 hover:text-gray-600'
-              >
-                {product.name}
-              </a>
-            </li>
-          </ol>
-        </nav>
-
         {/* Image gallery */}
         <div className='mt-6 max-w-2xl mx-auto sm:px-6 lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-3 lg:gap-x-8'>
           <div className='hidden aspect-w-3 aspect-h-4 rounded-lg overflow-hidden lg:block'>
             <ProgressiveImage
-              src={product.images[0].src}
+              src={productD[0].image.url || productD[0].assets[0].url}
               alt={product.images[0].alt}
               className='w-full h-full object-center object-cover'
             />
@@ -120,14 +99,14 @@ export const ProductDetails = () => {
           <div className='hidden lg:grid lg:grid-cols-1 lg:gap-y-8'>
             <div className='aspect-w-3 aspect-h-2 rounded-lg overflow-hidden'>
               <ProgressiveImage
-                src={product.images[1].src}
+                src={productD[0].image.url || productD[0].assets[0].url}
                 alt={product.images[1].alt}
                 className='w-full h-full object-center object-cover'
               />
             </div>
             <div className='aspect-w-3 aspect-h-2 rounded-lg overflow-hidden'>
               <ProgressiveImage
-                src={product.images[2].src}
+                src={productD[0].image.url}
                 alt={product.images[2].alt}
                 className='w-full h-full object-center object-cover'
               />
@@ -135,7 +114,7 @@ export const ProductDetails = () => {
           </div>
           <div className='aspect-w-4 aspect-h-5 sm:rounded-lg sm:overflow-hidden lg:aspect-w-3 lg:aspect-h-4'>
             <ProgressiveImage
-              src={product.images[3].src}
+              src={productD[0].image.url}
               alt={product.images[3].alt}
               className='w-full h-full object-center object-cover'
             />
@@ -146,7 +125,7 @@ export const ProductDetails = () => {
         <div className='max-w-2xl mx-auto pt-10 pb-16 px-4 sm:px-6 lg:max-w-7xl lg:pt-16 lg:pb-24 lg:px-8 lg:grid lg:grid-cols-3 lg:grid-rows-[auto,auto,1fr] lg:gap-x-8'>
           <div className='lg:col-span-2 lg:border-r lg:border-gray-200 lg:pr-8'>
             <h1 className='text-2xl font-extrabold tracking-tight text-gray-900 sm:text-3xl'>
-              {product.name}
+              {productD[0].name}
             </h1>
           </div>
 
@@ -322,7 +301,9 @@ export const ProductDetails = () => {
               <h3 className='sr-only'>Description</h3>
 
               <div className='space-y-6'>
-                <p className='text-base text-gray-900'>{product.description}</p>
+                <p className='text-base text-gray-900'>
+                  {productD[0].description}
+                </p>
               </div>
             </div>
 
